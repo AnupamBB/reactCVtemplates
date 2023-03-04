76357275dd426ee6904489a5120e5acfd16c96a3
Khalif <anupamborboruah100@gmail.com>
2023-03-05 00:32:47 +0530

    adding components

--- templates/src/App.js
+++ templates/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Template1 from "./templates/template1";
 
-function App() {
+function App(){
   return (
-    <div className="App">
-
+    <div>
+      <h1>hii</h1>
+        <Template1 />
     </div>
   );
 }
---
+++ templates/src/templates/template1.jsx
@@ -0,0 +1,13 @@
+import  React from 'react';
+import "./template1.css";
+
+const Template1 = () => {
+    return(
+        <div>
+            <h2>hi</h2>
+        </div>
+        
+    );
+};
+
+export default Template1;
