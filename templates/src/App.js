d34469881bd44a4de614fc436d26c91c1d0e8d67
Khalif <anupamborboruah100@gmail.com>
2023-03-05 12:59:02 +0530

    adding som div items

--- templates/src/App.js
+++ templates/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Template1 from "./templates/template1";
 
+
 function App(){
   return (
     <div>
-      <h1>hii</h1>
         <Template1 />
     </div>
   );
--- templates/src/templates/template1.jsx
+++ templates/src/templates/template1.jsx
@@ -1,13 +1,61 @@
-import  React from 'react';
+import React from "react";
 import "./template1.css";
 
 const Template1 = () => {
-    return(
-        <div>
-            <h2>hi</h2>
+  return (
+    <div>
+      <div className="page">
+        <div className="blocks">
+          <div className="leftBlock">
+            <div className="profile">
+              <img src="./images/profile.jpg" alt="" />
+            </div>
+            <div className="contact">Contact</div>
+            <div className="education">Education</div>
+            <div className="expertise">Expertise</div>
+            <div className="language">Language</div>
+          </div>
+          <div className="rightBlock">
+            <div className="header">
+              <h1>Anupam Bor Boruah</h1>
+            </div>
+            <div className="talgilne developer">React Developer</div>
+            <div className="rightText">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
+              itaque odit cum voluptate illum, iusto praesentium laboriosam
+              cumque, quod et repellat obcaecati. Vel, ducimus harum.
+            </div>
+            <div className="talgilne experience">Experience</div>
+            <div className="rightText">
+              <li className="year">year</li>
+              <div>company name | Anywhere st.. any city</div>
+              <div className="jobPositon">Job position here</div>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
+              itaque odit cum voluptate illum, iusto praesentium laboriosam
+              cumque, quod et repellat obcaecati. Vel, ducimus harum.
+            </div>
+            <div className="rightText">
+              <li className="year">year</li>
+              <div>company name | Anywhere st.. any city</div>
+              <div className="jobPositon">Job position here</div>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
+              itaque odit cum voluptate illum, iusto praesentium laboriosam
+              cumque, quod et repellat obcaecati. Vel, ducimus harum.
+            </div>
+            <div className="rightText">
+              <li className="year">year</li>
+              <div>company name | Anywhere st.. any city</div>
+              <div className="jobPositon">Job position here</div>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt
+              itaque odit cum voluptate illum, iusto praesentium laboriosam
+              cumque, quod et repellat obcaecati. Vel, ducimus harum.
+            </div>
+            <div className="talgilne projects">Projects</div>
+          </div>
         </div>
-        
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Template1;
